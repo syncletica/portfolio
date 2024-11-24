d118a0eff301dcc4a49517168fc2933658cf7a6f
Bonita McBride <bonitamcbride@gmail.com>
2024-11-24 14:11:46 -0600

    aria n styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    "./toolkit/index.html",
+    "./pwa/index.html",
+  ],
   theme: {
     extend: {},
   },
